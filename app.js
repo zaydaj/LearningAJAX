d1eb19b5641759f6b042069c9ba9a5b1ca093633
Zayda Janiak <zayda.janiak@aol.com>
2022-07-19 03:06:50 -0400

    addedrandomdogs

--- app.js
+++ app.js
@@ -4,3 +4,24 @@ async function getRandomDog() {
     const img = document.querySelector("#dog");
     img.src = res.data.message;
 }
+
+async function getDogByBreed(breed) { 
+try {
+    const url = `https://dog.ceo/api/breed/${breed}/images/random`
+    const res = await axios.get(url);
+    const img = document.querySelector("#dog");
+    img.src = res.data.message;
+} catch (e) {
+    alert('breed not found')
+}
+}
+
+const form = document.querySelector("#searchform");
+const input = document.querySelector("#search")
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+    // console.log('submit')
+    // console.log(input.value)
+getDogByBreed(input.value);
+input.value = '';
+})
